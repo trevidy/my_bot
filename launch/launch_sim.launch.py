78225966d39f06134ba452db914190f56b78eba6
trevidy <trevidytrevi@gmail.com>
2026-01-15 19:49:55 -0800

    implemented ros2_control. working on gamepad

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,6 +36,17 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package = "controller_manager",
+        executable = "spawner",
+        arguments = ["diff_cont"]
+    )
+
+    joint_broad_spawner = Node(
+        package = "controller_manager",
+        executable = "spawner",
+        arguments = ["joint_broad"]
+    )
 
 
     # Launch them all!
@@ -43,4 +54,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
